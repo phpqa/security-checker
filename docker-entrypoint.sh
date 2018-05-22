6cc680116df5ae7b41a449ea81e5c9d914432c69
MetalArend <metalarend@gmail.com>
2018-05-22 22:44:29 +0200

    Upgrade to the new Docker build

---
+++ docker-entrypoint.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+set -e
+
+isCommand() {
+  for cmd in \
+    "XXX"
+  do
+    if [ -z "${cmd#"$1"}" ]; then
+      return 0
+    fi
+  done
+
+  return 1
+}
+
+if [ "${1:0:1}" = "-" ]; then
+  set -- /sbin/tini -- php /vendor/bin/security-checker "$@"
+elif [ "$1" = "/vendor/bin/security-checker" ]; then
+  set -- /sbin/tini -- php "$@"
+elif [ "$1" = "security-checker" ]; then
+  set -- /sbin/tini -- php /vendor/bin/"$@"
+elif isCommand "$1"; then
+  set -- /sbin/tini -- php /vendor/bin/security-checker "$@"
+fi
+
+exec "$@"
